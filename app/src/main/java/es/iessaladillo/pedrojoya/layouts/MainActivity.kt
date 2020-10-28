020d81970f9a4c0332ffb4d110789bc4a019781c
Ana Castilla <ana.castilla1998@gmail.com>
2020-10-28 12:40:15 +0100

    Terminado

--- app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // TODO: USAR UNO DE LOS LAYOUTS DEFINIDOS PARA PROBARLO.
         //setContentView(R.layout.linearlayout_activity)
-          setContentView(R.layout.relativelayout_activity)
-      //  setContentView(R.layout.constraintlayout_activity)
+        // setContentView(R.layout.relativelayout_activity)
+       setContentView(R.layout.constraintlayout_activity)
     }
 }
